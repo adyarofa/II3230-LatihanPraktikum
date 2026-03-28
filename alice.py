57d3118864810253d9902fb675bb230b213a0a5a
WIjaksara Aptaluhung <19623024@std.stei.itb.ac.id>
2026-03-28 18:38:20 +0700

    feat: Alice mengirim pesan

---
+++ alice.py
@@ -0,0 +1,50 @@
+import socket
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from Crypto.Random import get_random_bytes
+
+text = input("Masukkan pesan yang ingin kamu kirim: ")
+tujuan = input("Masukkan IP Address penerima (harus berada di jaringan yang sama): ")
+port = 65432
+
+key = get_random_bytes(16)
+symmetrical = AES.new(key, AES.MODE_EAX)
+cipher_text, tag = symmetrical.encrypt_and_digest(text.encode("utf-8"))
+nonce = symmetrical.nonce
+print("Cipher Text:", cipher_text.hex())
+print("Tag:", tag.hex())
+print("Nonce:", nonce.hex())
+
+with open("public_key_bob.pem","rb") as f:
+    public_key_bob = RSA.import_key(f.read())
+    encrypted_symmetric_key = PKCS1_OAEP.new(public_key_bob).encrypt(key)
+    print("Encrypted Symmetric Key:", encrypted_symmetric_key.hex())
+
+hash = SHA256.new(text.encode("utf-8")).hexdigest()
+print("Hash:", hash)
+
+with open(".privatekey/private_key_alice.pem","rb") as f:
+    private_key_alice = RSA.import_key(f.read())
+    sign = PKCS1_OAEP.new(private_key_alice).encrypt(hash.encode("utf-8"))
+    print("Signature:", sign.hex())
+
+payload = {
+    "encrypted_symmetric_key": encrypted_symmetric_key.hex(),
+    "cipher_text": cipher_text.hex(),
+    "tag": tag.hex(),
+    "nonce": nonce.hex(),
+    "signature": sign.hex()
+}
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((tujuan, port))
+    s.sendall(str(payload).encode("utf-8"))
+    print("Pesan dikirim ke IP Address: ", tujuan)
+
+
+
+
+
+
+
