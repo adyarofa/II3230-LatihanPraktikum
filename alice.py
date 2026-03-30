dbaad05e47fb1349ecc76ebacb5ef949bf366265
WIjaksara Aptaluhung <19623024@std.stei.itb.ac.id>
2026-03-31 00:31:08 +0700

    fix: revisi payload

--- alice.py
+++ alice.py
@@ -1,12 +1,18 @@
 import socket
+import re
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
 from Crypto.Random import get_random_bytes
+from urllib.request import urlopen
 
 text = input("Masukkan pesan yang ingin kamu kirim: ")
-tujuan = input("Masukkan IP Address penerima (harus berada di jaringan yang sama): ")
+ip_tujuan = input("Masukkan IP Address penerima (harus berada di jaringan yang sama): ")
 port = 65432
+
+source = urlopen('http://checkip.dyndns.com')
+data = str(source.read())
+ip_asal = re.search(r"\d+\.\d+\.\d+\.\d+", data).group()
 
 key = get_random_bytes(16)
 symmetrical = AES.new(key, AES.MODE_EAX)
@@ -30,21 +36,19 @@ with open(".privatekey/private_key_alice.pem","rb") as f:
     print("Signature:", sign.hex())
 
 payload = {
+    "source_ip": ip_asal,
+    "destination_ip": ip_tujuan,
     "encrypted_symmetric_key": encrypted_symmetric_key.hex(),
     "cipher_text": cipher_text.hex(),
     "tag": tag.hex(),
     "nonce": nonce.hex(),
-    "signature": sign.hex()
+    "signature": sign.hex(),
+    "hash_algorithm": "SHA256",
+    "symmetric_algorithm": "AES256",
+    "asymmetric_algorithm": "RSA"
 }
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((tujuan, port))
+    s.connect((ip_tujuan, port))
     s.sendall(str(payload).encode("utf-8"))
-    print("Pesan dikirim ke IP Address: ", tujuan)
-
-
-
-
-
-
-
+    print("Pesan dikirim dari IP Address ", ip_asal, " ke IP Address ", ip_tujuan)
